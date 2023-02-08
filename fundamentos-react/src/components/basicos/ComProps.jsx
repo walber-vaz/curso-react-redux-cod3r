9526b5e1ce90506948378051b52d668412605426
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-08 08:33:27 -0300

    chore: used jsx fragment

--- fundamentos-react/src/components/basicos/ComProps.jsx
+++ fundamentos-react/src/components/basicos/ComProps.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 export default function ComProps(props) {
   const { titulo, aluno, nota } = props;
+  const aprov = nota >= 7 ? 'aprovado' : 'reprovado';
   return (
-    <div>
+    <>
       <h2>{titulo}</h2>
       <h3>
         <strong>{aluno}</strong>
       </h3>
       <p>
-        tem nota <strong>{nota}</strong>
+        tem nota <strong>{nota}</strong> e foi <strong>{aprov}</strong>
       </p>
-    </div>
+    </>
   );
 }
