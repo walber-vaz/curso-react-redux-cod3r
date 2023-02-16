dd3d059e5fa67dba5e0714b360930928f208f75c
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-16 08:42:21 -0300

    chore: indirection for children in fhader used useState

--- fundamentos-react/src/components/comunicacao/Indireata.jsx
+++ fundamentos-react/src/components/comunicacao/Indireata.jsx
@@ -1,15 +1,24 @@
 /* eslint-disable react/jsx-no-bind */
-import React from 'react';
+import React, { useState } from 'react';
 import IndiretaChildren from './IndiretaChildren';
 
 function Indireata() {
-  function fornecerInformacaoes(nome, idade, nerd) {
-    console.log(nome, idade, nerd);
+  const [nome, setNome] = useState('?');
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
+
+  function fornecerInformacaoes(nomeChildren, idadeChildren, nerdChildren) {
+    setIdade(idadeChildren);
+    setNerd(nerdChildren);
+    setNome(nomeChildren);
   }
 
   return (
     <div>
       <h1>Pai</h1>
+      <span>{nome}</span>
+      <span> {idade}</span>
+      <span> {nerd ? 'Verdadeiro' : 'Falso'}</span>
       <IndiretaChildren onInfo={fornecerInformacaoes} />
     </div>
   );
--- fundamentos-react/src/components/comunicacao/IndiretaChildren.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaChildren.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function IndiretaChildren({ onInfo }) {
+  const min = 50;
+  const max = 100;
+  const gerarIdade = () => parseInt(Math.random() * (max - min), 10) + min;
   return (
     <div>
       <p>Filho</p>
-      <button type="button" onClick={() => onInfo('w2k', 32, true)}>
+      <button type="button" onClick={() => onInfo('w2k', gerarIdade(), true)}>
         Fornecer informações
       </button>
     </div>
