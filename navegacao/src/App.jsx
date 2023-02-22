72e7a38ee0e574ae9d54e2a8e393f2bd9df4a722
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-22 11:37:36 -0300

    chore: clean project

---
+++ navegacao/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App</h1>
+      <p>tutorial react router dom</p>
+    </div>
+  );
+};
+
+export default App;
