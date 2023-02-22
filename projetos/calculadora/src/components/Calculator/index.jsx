10ebf0517cacdd772e8667563dbe5e74021afb90
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-22 09:54:43 -0300

    feat: create function logica view digit clicked in display

--- projetos/calculadora/src/components/Calculator/index.jsx
+++ projetos/calculadora/src/components/Calculator/index.jsx
@@ -1,42 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import Display from "../Display";
 
 import "./style.css";
 
 function Calculator() {
+  const [displayValue, setDisplayValue] = useState(0);
+  const [clearDisplay, setClearDisplay] = useState(false);
+  const [operation, setOperation] = useState(null);
+  const [values, setValues] = useState([0, 0]);
+  const [current, setCurrent] = useState(0);
+
   function cleanMemory() {
-    console.log("limpar");
+    setDisplayValue(0);
+    setClearDisplay(false);
+    setOperation(null);
+    setValues([0, 0]);
+    setCurrent(0);
   }
 
-  function setOperation(operation) {
+  function setOperationFunc(operation) {
     console.log(operation);
   }
 
   function addDigit(n) {
-    console.log(n);
+    if (n === "." && displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplayValue = displayValue === 0 || clearDisplay;
+    const currentValue = clearDisplayValue ? "" : displayValue;
+    const displayValueUpdated = currentValue + n;
+    setDisplayValue(displayValueUpdated);
+    setClearDisplay(false);
+
+    if (n !== ".") {
+      const newValue = parseFloat(displayValueUpdated);
+      const valuesUpdated = [...values];
+      valuesUpdated[current] = newValue;
+      setValues(valuesUpdated);
+      console.log(valuesUpdated);
+    }
   }
 
   return (
     <div className="calculator">
-      <Display value={100} />
+      <Display value={displayValue} />
       <Button label="AC" click={cleanMemory} triple />
-      <Button label="/" click={setOperation} operation />
+      <Button label="/" click={setOperationFunc} operation />
       <Button label="7" click={addDigit} />
       <Button label="8" click={addDigit} />
       <Button label="9" click={addDigit} />
-      <Button label="*" click={setOperation} operation />
+      <Button label="*" click={setOperationFunc} operation />
       <Button label="4" click={addDigit} />
       <Button label="5" click={addDigit} />
       <Button label="6" click={addDigit} />
-      <Button label="-" click={setOperation} operation />
+      <Button label="-" click={setOperationFunc} operation />
       <Button label="1" click={addDigit} />
       <Button label="2" click={addDigit} />
       <Button label="3" click={addDigit} />
-      <Button label="+" click={setOperation} operation />
+      <Button label="+" click={setOperationFunc} operation />
       <Button label="0" click={addDigit} double />
       <Button label="." click={addDigit} />
-      <Button label="=" click={setOperation} operation />
+      <Button label="=" click={setOperationFunc} operation />
     </div>
   );
 }
